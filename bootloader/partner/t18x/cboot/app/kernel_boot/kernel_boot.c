176975ae72256c2a0d0840a9ab37d02a4dec723f
Joe Swantek <joseph.swantek@northwestern.edu>
2020-11-10 16:36:54 -0600

    SAGE-515: update A/B register before attempted kernel load; reset on failure
    With out this change kernel loading issues did not rotate the A/B counter
    in the scratch register, essentially disabling A/B recovery.
    
    Additionally, perform system reset instead of wait for watchdog trigger.
    
    This change is derived from an NVidia recommended change and back-ported
    from 32.5.0:
    Source: https://forums.developer.nvidia.com/t/failed-bootloader-watchdog-recovery/154380

--- bootloader/partner/t18x/cboot/app/kernel_boot/kernel_boot.c
+++ bootloader/partner/t18x/cboot/app/kernel_boot/kernel_boot.c
@@ -179,10 +179,19 @@ tegrabl_error_t load_and_boot_kernel(struct tegrabl_kernel_bin *kernel)
 #endif
 
 	err = tegrabl_load_kernel_and_dtb(kernel, &kernel_entry_point,
-									  &kernel_dtb, &callbacks, NULL, 0);
+						  &kernel_dtb, &callbacks, NULL, 0);
+
+	/*
+	 * Update smd if a/b retry counter changed
+	 * The slot priorities are rotated here too,
+	 * in case kernel or kernel-dtb load failed.
+	*/
+	tegrabl_a_b_update_smd();
+
 	if (err != TEGRABL_NO_ERROR) {
 		TEGRABL_SET_HIGHEST_MODULE(err);
-		pr_error("kernel boot failed\n");
+		pr_error("kernel boot failed, will reset.\n");
+		tegrabl_reset();
 		return err;
 	}
 
@@ -199,9 +208,6 @@ tegrabl_error_t load_and_boot_kernel(struct tegrabl_kernel_bin *kernel)
 #if defined(IS_T186)
 	tegrabl_profiler_record("kernel_boot exit", 0, DETAILED);
 #endif
-
-	/* Update smd if a/b retry counter changed */
-	tegrabl_a_b_update_smd();
 
 	pr_info("Kernel EP: %p, DTB: %p\n", kernel_entry_point, kernel_dtb);
 
@@ -340,4 +346,3 @@ STATIC_COMMAND_START
 STATIC_COMMAND("boot", "kernel boot", &kernel_boot_cmd)
 STATIC_COMMAND_END(kernel_boot_cmd);
 #endif
-
