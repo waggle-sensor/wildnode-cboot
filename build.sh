3e4c5977222fdd54c9cca4effafa0fadefc10bed
Joe Swantek <joseph.swantek@northwestern.edu>
2021-03-24 12:21:00 -0500

    SAGE-746: add CI and use release_manifest

--- build.sh
+++ build.sh
@@ -2,12 +2,13 @@
 
 function print_help() {
   echo """
-usage: build.sh -t <L4T toolchain> [-f]
+usage: build.sh -t <L4T toolchain> [-f] [-r]
 
 Creates the CBoot (lk; little kernel) binary.
 
   -t : path to the Jetson Driver Linux toolchain (i.e. parent directory to bin/aarch64-linux-gnu-)
   -z : (optional) leave the make output directory instead of cleaning it up
+  -r : (optional) create a release using `release_manifest`. Ignores all other input parameters.
   -f : force the build to proceed (debugging only) without checking for tagged commit
 """
 }
@@ -28,12 +29,18 @@ trap cleanup EXIT
 # input arguments
 ARG_TOOLCHAIN=
 ARG_CLEAN=1
+ARG_CREATE_RELEASE=
 FORCE=
-while getopts "t:zf?" opt; do
+while getopts "t:zrf?" opt; do
   case $opt in
     t) ARG_TOOLCHAIN=$(realpath $OPTARG)
       ;;
     z) ARG_CLEAN=
+      ;;
+    r) ARG_CREATE_RELEASE=1
+      echo "** CREATING RELEASE **"
+      C_RELEASE_FILE=$(realpath ./release_manifest)
+      ARG_TOOLCHAIN=
       ;;
     f) # force build
       echo "** Force build: ignore tag depth check **"
@@ -55,6 +62,26 @@ if [[ -z "${FORCE}" && "${TAG_DEPTH}_" != "0_" ]]; then
   echo "  The current git commit has not been tagged. Please create a new tag first to ensure a proper unique version number."
   echo "  Use -f to ignore error (for debugging only)."
   exit 1
+fi
+
+# Peform steps to create release using the release manifest
+if [ -n "${ARG_CREATE_RELEASE}" ]; then
+    source "${C_RELEASE_FILE}"
+
+    echo "Download build toolchain (${R_TOOLCHAIN} [${R_TOOLCHAIN_MD5}])"
+    wget -c "${R_TOOLCHAIN}"
+    DMD5=$(md5sum $(basename $R_TOOLCHAIN) | cut -d' ' -f1)
+    if [ "${DMD5}" != "${R_TOOLCHAIN_MD5}" ]; then
+        echo "Error: Unable to process release. Download [${R_TOOLCHAIN}]" \
+              "checksum [md5: ${DMD5}] does not match expected value [md5: ${R_TOOLCHAIN_MD5}]." \
+              "Exiting."
+        exit 1
+    fi
+    tar xf $(basename $R_TOOLCHAIN)
+    ARG_TOOLCHAIN=$(realpath $(basename $R_TOOLCHAIN .tar.xz))
+
+    echo "Creating release [${C_RELEASE_FILE}]"
+    echo " - Toolchain:  ${ARG_TOOLCHAIN}"
 fi
 
 # constant string definitions
